008c267b619bd22b7b8c7ca01e289b1529263f85
Painttt46 <vpaint345@gmail.com>
2026-02-06 16:54:49 +0700

    SSV

--- src/views/daily_work/TaskList.vue
+++ src/views/daily_work/TaskList.vue
@@ -555,7 +555,7 @@ export default {
     workStatusFilterOptions() {
       const options = [
         { label: 'ทั้งหมด', value: null },
-        { label: '-', value: null }
+        { label: '-', value: 'no_status'}
       ]
       
       // เอาเฉพาะ statuses ที่มีใน taskWorks
